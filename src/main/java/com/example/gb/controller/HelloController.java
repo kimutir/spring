4a6ba8df547813a39dd5b731507552f0c46d971f
Timur Kim <kimutir@gmail.com>
2024-01-15 19:53:08 +1100

    done

---
+++ src/main/java/com/example/gb/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.gb.controller;
+
+import com.example.gb.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return helloService.sayHello();
+    }
+
+}
---
+++ src/main/java/com/example/gb/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.example.gb.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String sayHello() {
+        return "Hello, anonymous";
+    }
+
+}
